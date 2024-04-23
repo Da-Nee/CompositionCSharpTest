91608d23cc9351274a201abcf201766ae4fa08c5
Felipe Yohan Sapio <yohansapio@rocketmail.com>
2024-04-23 00:12:58 -0300

    Composition study

---
+++ CompositionExec/Entities/Order.cs
@@ -0,0 +1,65 @@
+﻿using CompositionExec.Entities.Enums;
+using System.Globalization;
+using System.Text;
+
+namespace CompositionExec.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; } = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Orders { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+        public Product Product { get; set; }
+
+        public Order() { }
+
+        public Order(OrderStatus status, Client client)
+        {
+            Status = status;
+            Client = client;
+        }
+
+        public void addItem(OrderItem item)
+        {
+            Orders.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Orders.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0;
+            foreach (OrderItem item in Orders)
+            {
+                sum += item.SubTotal();
+            }
+
+            return sum;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("ORDER SUMMARY:");
+            sb.AppendLine("Order moment: " + Moment);
+            sb.AppendLine("Order status: " + Status);
+            sb.Append("Client: " + Client.Name);
+            sb.Append(" " + Client.BirthDate.ToString("dd/MM/yyyy"));
+            sb.AppendLine(" - " + Client.Email);
+            sb.AppendLine("Order items: ");
+
+            foreach (OrderItem orderItem in Orders)
+            {
+                sb.AppendLine(orderItem.ToString());
+            }
+
+            sb.AppendLine("Total price: $" + Total().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ CompositionExec/Program.cs
@@ -0,0 +1,47 @@
+﻿using CompositionExec.Entities;
+using CompositionExec.Entities.Enums;
+using System.Globalization;
+
+namespace CompositionExec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime brthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(name, email, brthDate);
+
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());            
+
+            Order order = new Order(status, client);
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string pName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double pPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int qtd = int.Parse(Console.ReadLine());                
+                Product product = new Product(pName, pPrice);
+                OrderItem orderItem = new OrderItem(qtd, pPrice, product);
+                order.addItem(orderItem);
+            }
+                        
+            Console.WriteLine();
+            Console.WriteLine(order);
+        }
+    }
+}
